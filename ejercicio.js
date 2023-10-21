a1f68928b43294ea23580a960a8ab2dfd03a081d
MendozaMarcelino <marcelino.mendoza@cecar.edu.co>
2023-10-20 20:38:24 -0500

    envie el taller 5

---
+++ ejercicio.js
@@ -0,0 +1,142 @@
+var imagen = new Image();
+
+
+imagen.src = "imagenes/exito.webp"; 
+
+imagen.width = 300;
+
+imagen.height = 200;
+
+document.body.appendChild(imagen);
+
+imagen.style.marginRight = "13px";
+
+    
+
+
+
+
+
+
+var imagen = new Image();
+
+imagen.src = "imagenes/estadiometropolitano.jpg";
+
+imagen.width = 300;
+
+imagen.height = 200;
+
+document.body.appendChild(imagen);
+
+imagen.style.marginRight = "13px";
+
+
+
+
+
+
+var imagen = new Image();          imagen.src = "imagenes/netflix.jpg";
+
+imagen.width = 300;
+
+imagen.height = 200;
+
+document.body.appendChild(imagen);
+
+imagen.style.marginRight = "13px";
+
+
+
+
+
+
+
+
+var columna = document.createElement("div");
+
+columna.style.display = "flex";
+
+columna.style.flexDirection = "column";
+
+
+
+
+
+var imagen1 = new Image();
+
+imagen1.src = "";
+
+imagen1.width = 300;
+
+imagen1.height = 200;
+
+imagen1.style.marginBottom = "10px"; 
+
+
+
+
+
+var columna = document.createElement("div");
+columna.style.display = "flex";
+
+
+
+
+
+var imagen1 = new Image();
+imagen1.src = "imagenes/BanderaColombia33.jpg";
+imagen1.width = 300;
+imagen1.height = 200;
+
+
+columna.appendChild(imagen1);
+
+
+var espacio = document.createElement("div");
+espacio.style.width = "13px"; 
+columna.appendChild(espacio);
+
+
+
+
+
+
+var imagen2 = new Image();
+imagen2.src = "imagenes/facebook.png";
+imagen2.width = 300;
+imagen2.height = 200;
+
+
+columna.appendChild(imagen2);
+
+
+document.body.appendChild(columna);
+
+
+var espacio = document.createElement("div");
+espacio.style.width = "13px"; 
+columna.appendChild(espacio);
+
+
+
+
+var imagen2 = new Image();
+imagen2.src = "imagenes/avion.jpg";
+
+imagen2.width = 300;
+
+imagen2.height = 200;
+
+
+
+
+columna.appendChild(imagen2);
+
+
+document.body.appendChild(columna);
+
+
+
+
+
+
